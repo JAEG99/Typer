ddf6d3e331ea7f2d0e207ae36e680249ad25a53d
James Gilliland <j.a.e.gilliland@outlook.com>
2023-09-03 15:15:34 +0100

    Added functions to the java and the array

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const paragraphs = [
+    "The sun dipped below the horizon, casting long shadows across the tranquil landscape. The air was cool and crisp, carrying with it the faint scent of pine trees. As the stars began to twinkle in the evening sky, a sense of calm settled over the world.",
+    "The bustling city streets were alive with the sounds of honking horns and chattering pedestrians. Neon signs illuminated the night, creating a colorful tapestry of light and motion. Amidst the chaos, a sense of excitement permeated the air, promising adventures yet to be discovered.",
+    "In a quiet corner of the library, an old book sat on a dusty shelf, its pages yellowed with age. As the librarian turned its delicate pages, a world of forgotten stories and ancient wisdom unfolded. Each word seemed to carry a piece of history, waiting to be cherished once more.",
+    "The waves crashed against the shore with a rhythmic cadence, as seagulls soared gracefully through the salty breeze. The sand beneath my feet was warm and soothing, a reminder of nature's embrace. With every step, I felt a deeper connection to the vast ocean before me."
+  ];
+
+  const typingText = document.querySelector(".typed-text p"),
+    inpField = document.querySelector(".typing-input"),
+    tryAgainBtn = document.querySelector("button"),
+    mistakeTag = document.querySelector(".errors span"),
+    wpmTag = document.querySelector(".wpm span"),
+    cmpTag = document.querySelector(".cmp span");
+
+    function loadParagraph() {
+
+    }
+
+    function initTyping() {
+
+    }
+
+    function initTimer() {
+
+    }
+
+    function resetGame() {
+
+    }
+
+    loadParagraph();
+
