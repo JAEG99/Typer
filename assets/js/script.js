4d5c4c1bc33dc0da58e29da9a9faceb2939b7c7d
James Gilliland <j.a.e.gilliland@outlook.com>
2023-10-17 22:27:22 +0100

    Add high score
    Add Difficulty
    
    Fix footer
    
    Improve responsivness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,166 +1,213 @@
+// Create an object to store high scores by difficulty
+const highScores = {
+  easy: [],
+  medium: [],
+  hard: [],
+};
+
+// Function to save a high score for a specific difficulty
+const saveHighScore = function (score, difficulty) {
+  const scores = highScores[difficulty] || [];
+
+  const newScore = {
+    score: score,
+    date: new Date().toLocaleString(),
+  };
+
+  scores.push(newScore);
+  scores.sort((a, b) => b.score - a.score); // Sort scores in descending order
+  highScores[difficulty] = scores;
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+};
+
+// Function to display high scores for a specific difficulty
+const displayHighScores = function (difficulty) {
+  const scores = highScores[difficulty] || [];
+  const highScoresList = document.getElementById("highScoresList");
+
+  // Clear the previous high scores
+  highScoresList.innerHTML = "";
+
+  scores.forEach((score, index) => {
+    const li = document.createElement("li");
+    li.textContent = `${index + 1}. ${score.score} WPM - ${score.date}`;
+    highScoresList.appendChild(li);
+  });
+};
+
 if (window.location.pathname.endsWith("game.html")) {
-const paragraphs = [
-  // Array of paragraphs for typing challenge
-    "In the heart of the dense, emerald forest, ancient trees stood tall, their branches intertwined in a dance that had lasted for centuries. Shafts of golden sunlight pierced through the thick canopy, illuminating the moss-covered ground below. As I walked deeper into this enchanted woodland, I felt a sense of wonder and reverence for the natural world.",
-    "Amidst the bustling streets of the vibrant metropolis, skyscrapers reached for the heavens, their glass facades reflecting the urban symphony of life. The city's diverse inhabitants hurried along, each with their own story to tell. It was a place where dreams were born and ambitions soared, a microcosm of human existence.",
-    "High atop the rugged mountains, a lonely mountain goat navigated treacherous cliffs with unmatched grace. The air was thin and crisp, and the view from this lofty perch was breathtaking. I marveled at the sheer grandeur of the world below, a reminder of nature's indomitable spirit.",
-    "In the quiet solitude of a dimly lit library, rows upon rows of ancient books filled the shelves. Each tome held the collective knowledge and wisdom of generations long past. As I opened one of these weathered volumes, I was transported to distant lands and eras, where history came alive through the printed word."
-  ];
+  const paragraphs = {
+    easy: [
+      // Easy difficulty paragraphs
+      "Balloons are round and colorful. They float up high when we let them go. We tie them with strings and watch them dance in the sky. At parties, we love balloons because they make us smile.",
+      "Butterflies are pretty insects. They have bright wings with patterns. They flutter from flower to flower, sipping sweet nectar. Watching them in the garden is like seeing living rainbows.",
+      "Fireworks are like magic in the night sky. They go boom and burst into dazzling colors. Red, blue, green, and gold light up the darkness. Celebrations wouldn't be complete without fireworks.",
+      "Seashells are treasures from the ocean. They come in all shapes and sizes. When we go to the beach, we search for them in the sand. Each shell has a story of the sea to tell."
+    ],
+    medium: [
+      // Medium difficulty paragraphs
+      "In a cozy, little cottage by the edge of a tranquil lake, an old fisherman sat quietly with his fishing rod. The sun painted the sky with warm hues of orange and pink as he patiently waited for a fish to bite.",
+      "Under the shade of a massive oak tree, children played, their laughter echoing through the park. Birds sang in harmony as a gentle breeze rustled the leaves. It was a perfect day for a picnic with friends and family.",
+      "The ancient castle stood on the hill, its stone walls telling tales of centuries gone by. Tourists explored its winding corridors, captivated by the history that lingered in every nook and cranny. Ghostly legends whispered through the air.",
+      "Amidst the emerald fields, a rainbow stretched across the sky, its vibrant colors casting a magical glow. A gentle rain was falling, and the scent of fresh earth filled the air. Nature was painting a masterpiece, and all was right in the world."
+    ],
+    hard: [
+      // Hard difficulty paragraphs
+      "Amidst the cacophony of a bustling metropolis, a solitary street musician plays a haunting melody on his violin, captivating the hearts of passersby. The sound reverberates through the concrete canyons, echoing the collective yearning of a city that never sleeps.",
+      "In the intricate labyrinth of life, we often find ourselves at crossroads, making choices that define our destiny. It is in these moments that our true character shines. The decisions we make ripple through time, shaping the narrative of our existence.",
+      "The enigmatic beauty of the cosmos has long fascinated astronomers, who dedicate their lives to unraveling the mysteries of the universe, one celestial body at a time. They peer through the telescopes, studying distant galaxies, and ponder the origins of dark matter, pushing the boundaries of human understanding.",
+      "A virtuoso pianist takes center stage, captivating the audience with an electrifying performance that resonates deep within the souls of all who bear witness to his artistry. His fingers dance across the ivory keys, invoking emotions that transcend language, leaving the world in awe of the power of music."
+    ]
+  };
 
-  const typingText = document.querySelector(".typed-text p"), // Element to display the typing text
-  inpField = document.querySelector(".typing-input"), // Input field where the user types
-  tryAgainBtn = document.querySelector("button"), // Restart button
-  timeTag = document.querySelector(".time-left span b"), // Element to display time remaining
-  mistakeTag = document.querySelector(".errors span"), // Element to display mistake count
-  wpmTag = document.querySelector(".wpm span"), // Element to display words per minute
-  cpmTag = document.querySelector(".cpm span"); // Element to display characters per minute
-let timer,
-  maxTime = 60, // Maximum time for the typing challenge in seconds
-  timeLeft = maxTime, // Current time remaining
-  charIndex = (mistakes = isTyping = 0), // Variables for tracking characters, mistakes, and typing status
-  totalCharacters = 0; // Total characters in the typing text
+  const typingText = document.querySelector(".typed-text p"),
+    inpField = document.querySelector(".typing-input"),
+    tryAgainBtn = document.querySelector("button"),
+    timeTag = document.querySelector(".time-left span b"),
+    mistakeTag = document.querySelector(".errors span"),
+    wpmTag = document.querySelector(".wpm span"),
+    cpmTag = document.querySelector(".cpm span");
 
-  // Function to load a random paragraph for the typing challenge
-function loadParagraph() {
-  const ranIndex = Math.floor(Math.random() * paragraphs.length);
-  typingText.innerHTML = "";
-  paragraphs[ranIndex].split("").forEach((char) => {
-    let span = `<span>${char}</span>`;
-    typingText.innerHTML += span;
-    totalCharacters++; 
+  let timer,
+    maxTime = 60,
+    timeLeft = maxTime,
+    charIndex = (mistakes = isTyping = 0),
+    totalCharacters = 0;
+
+  let timerReachedZero = false;
+
+  const difficultySelect = document.getElementById("difficulty");
+  let selectedDifficulty = "easy";
+
+  difficultySelect.addEventListener("change", function () {
+    selectedDifficulty = difficultySelect.value;
+    resetGame();
   });
-  typingText.querySelectorAll("span")[0].classList.add("active");
-  document.addEventListener("keydown", () => inpField.focus());
-  typingText.addEventListener("click", () => inpField.focus());
-}
-let timerReachedZero = false;
 
-// Function to calculate and display accuracy
-function calculateAccuracy() {
-  const accuracyLabel = document.querySelector(".errors p");
-  const accuracy = ((totalCharacters - mistakes) / totalCharacters) * 100;
+  const loadParagraph = function () {
+    const paragraphArray = paragraphs[selectedDifficulty];
+    const ranIndex = Math.floor(Math.random() * paragraphArray.length);
+    typingText.innerHTML = "";
+    paragraphs[selectedDifficulty][ranIndex].split("").forEach((char) => {
+      let span = `<span>${char}</span>`;
+      typingText.innerHTML += span;
+      totalCharacters++;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+  };
 
-  accuracyLabel.textContent = "Accuracy:";
-
-  if (timerReachedZero) {
-    accuracyLabel.textContent = "Mistakes:";
-  }
-
-  mistakeTag.textContent = `${accuracy.toFixed(2)}%`;
-}
-
-// Function to handle typing and update stats
-function initTyping() {
-  let characters = typingText.querySelectorAll("span");
-  let typedChar = inpField.value.split("")[charIndex];
-  if (charIndex < characters.length - 1 && timeLeft > 0) {
-    if (!isTyping) {
-      timer = setInterval(initTimer, 1000);
-      isTyping = true;
-    }
-    if (typedChar == null) {
-      if (charIndex > 0) {
-        charIndex--;
-        if (characters[charIndex].classList.contains("incorrect")) {
-          mistakes--;
-        }
-        characters[charIndex].classList.remove("correct", "incorrect");
-      }
-    } else {
-      if (characters[charIndex].innerText == typedChar) {
-        characters[charIndex].classList.add("correct");
-      } else {
-        mistakes++;
-        characters[charIndex].classList.add("incorrect");
-      }
-      charIndex++;
-    }
-    characters.forEach((span) => span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
-    let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
-
-    wpmTag.innerText = wpm;
-    mistakeTag.innerText = mistakes;
-    cpmTag.innerText = charIndex - mistakes;
-  } else {
+  const resetGame = function () {
+    loadParagraph();
     clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
     inpField.value = "";
-    if (timeLeft === 0) {
-      calculateAccuracy(document.getElementById("inputText"));
-    }
-  }
-}
-
-
-// Function to calculate accuracy
-function calculateAccuracy() {
-  const accuracyLabel = document.querySelector(".errors p");
-  const accuracy = ((charIndex - mistakes) / charIndex) * 100; 
-
-  accuracyLabel.textContent = "Mistakes:";
-
-  if (timerReachedZero) {
-    accuracyLabel.textContent = "Accuracy:";
-  }
-
-  mistakeTag.textContent = `${accuracy.toFixed(2)}%`;
-}
-
-// Function to play the timer sound
-function playTimerSound() {
-  const timerSound = document.getElementById("timerSound");
-  timerSound.play();
-}
-
-
-// Function to calculate and update the timer
-function initTimer() {
-  if (timeLeft > 0) {
-    timeLeft--;
     timeTag.innerText = timeLeft;
-    let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-    wpmTag.innerText = wpm;
-  } else {
-    clearInterval(timer);
-    timerReachedZero = true;
-    calculateAccuracy(document.getElementById("inputText"));
-    playTimerSound();
-
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
   
-  }
-}
-
-// Function to reset the typing challenge
-function resetGame() {
-  loadParagraph();
-  clearInterval(timer);
-  timeLeft = maxTime;
-  charIndex = mistakes = isTyping = 0;
-  inpField.value = "";
-  timeTag.innerText = timeLeft;
-  wpmTag.innerText = 0;
-  mistakeTag.innerText = 0;
-  cpmTag.innerText = 0;
-}
-
-// Event listener for handling backspace key (prevent default behavior)
-if (inpField) { // Check if inpField exists on the current page
-  inpField.addEventListener("keydown", function (e) {
-    if (e.keyCode === 8) {
-      e.preventDefault();
+    if (timeLeft === 0) {
+      // Calculate accuracy after the timer reaches 0
+      calculateAccuracy(document.getElementById("inputText"));
+       
+      saveHighScore(wpm, selectedDifficulty);
+      displayHighScores(selectedDifficulty); // Display updated high scores here
     }
-  });
+  };
+
+  const calculateAccuracy = function () {
+    const accuracyLabel = document.querySelector(".errors p");
+    const accuracy = ((charIndex - mistakes) / charIndex) * 100;
+
+    accuracyLabel.textContent = "Mistakes:";
+
+    if (timerReachedZero) {
+      accuracyLabel.textContent = "Accuracy:";
+    }
+
+    mistakeTag.textContent = `${accuracy.toFixed(2)}%`;
+  };
+
+  const playTimerSound = function () {
+    const timerSound = document.getElementById("timerSound");
+    timerSound.play();
+  };
+
+  const initTyping = function () {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+      if (!isTyping) {
+        timer = setInterval(initTimer, 1000);
+        isTyping = true;
+      }
+      if (typedChar == null) {
+        if (charIndex > 0) {
+          charIndex--;
+          if (characters[charIndex].classList.contains("incorrect")) {
+            mistakes--;
+          }
+          characters[charIndex].classList.remove("correct", "incorrect");
+        }
+      } else {
+        if (characters[charIndex].innerText == typedChar) {
+          characters[charIndex].classList.add("correct");
+        } else {
+          mistakes++;
+          characters[charIndex].classList.add("incorrect");
+        }
+        charIndex++;
+      }
+      characters.forEach((span) => span.classList.remove("active"));
+      characters[charIndex].classList.add("active");
+      let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+      wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+      wpmTag.innerText = wpm;
+      mistakeTag.innerText = mistakes;
+      cpmTag.innerText = charIndex - mistakes;
+    } else {
+      clearInterval(timer);
+      inpField.value = "";
+      if (timeLeft === 0) {
+        calculateAccuracy(document.getElementById("inputText"));
+        saveHighScore(wpm, selectedDifficulty);
+        displayHighScores(selectedDifficulty); // Display updated high scores
+      }
+    }
+  };
+
+  const initTimer = function () {
+    if (timeLeft > 0) {
+      timeLeft--;
+      timeTag.innerText = timeLeft;
+      let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+      wpmTag.innerText = wpm;
+    } else {
+      clearInterval(timer);
+      timerReachedZero = true;
+      calculateAccuracy(document.getElementById("inputText"));
+      playTimerSound();
+
+      // Check if timeLeft is zero here and display high scores
+      if (timeLeft === 0) {
+        calculateAccuracy(document.getElementById("inputText"));
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        saveHighScore(wpm, selectedDifficulty);
+        displayHighScores(selectedDifficulty); // Display updated high scores
+      }
+    }
+  };
+
+  loadParagraph();
+
+  inpField.addEventListener("input", initTyping);
+  tryAgainBtn.addEventListener("click", resetGame);
 }
 
-// Load an initial paragraph when the page loads
-loadParagraph();
-
-// Event listeners for input and restart button
-inpField.addEventListener("input", initTyping);
-tryAgainBtn.addEventListener("click", resetGame);
-
-}
 // JS for the button on the landing page
 document.addEventListener("DOMContentLoaded", function () {
   const startButton = document.getElementById("startButton");
@@ -171,3 +218,5 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+displayHighScores(selectedDifficulty);
